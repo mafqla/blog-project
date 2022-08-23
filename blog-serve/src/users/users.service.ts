82ecdfd0d34aa3cf45245759796c67d8d45a159a
mafqla <1692285748@qq.com>
2022-08-23 22:33:20 +0800

    测试用户是否存在

--- blog-serve/src/users/users.service.ts
+++ blog-serve/src/users/users.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common'
-import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectModel } from '@nestjs/sequelize'
+import { User } from './model/user.model'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user'
-  }
+  constructor(
+    @InjectModel(User)
+    private readonly userRepository: typeof User
+  ) {}
 
-  findAll() {
-    return `This action returns all users`
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`
+  // 根据用户名查询用户是否存在
+  async getUsersByUserName(userName: string) {
+    try {
+      const users = await this.userRepository.findAll<User>({
+        where: {
+          userName: userName
+        }
+      })
+      if (users) {
+        return {
+          code: 200,
+          data: users,
+          message: '查询成功'
+        }
+      }
+    } catch (err) {
+      return {
+        code: 404,
+        data: err,
+        message: '用户不存在'
+      }
+    }
   }
 }
